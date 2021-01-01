a5753db4af709167c5cb98dd520803cd19822b71
Joel Juhela <joel.juhela@gmail.com>
2021-01-01 15:09:46 +0200

    Naive privacy link search implemented

---
+++ content.js
@@ -0,0 +1,27 @@
+window.onload=() => findPrivacyLink();
+
+
+const findPrivacyLink = () => {
+    /* Get all links in a document */
+    console.log("start search")
+    const links = Array.from(document.links);
+
+    const privacyLinks = links.filter(containsPrivacy);
+
+    console.log(privacyLinks);
+    chrome.runtime.sendMessage({
+        payload:{
+            linksFound:privacyLinks.length > 0,
+            links: privacyLinks
+        }
+    });
+}
+
+const containsPrivacy = (item) => {
+    /* Helper function for checking if link items href or innertext has the string "privacy" in it */
+
+    const href = item.href.match(/privacy/i);
+    const innerText = item.innerText.match(/privacy/i);
+
+    return  href || innerText
+}
